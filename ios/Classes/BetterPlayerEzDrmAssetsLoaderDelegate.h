cc45791d8370ffc865c2e4a8246ab9416574fbb5
solog80 <162872800+solog80@users.noreply.github.com>
2026-01-01 10:42:04 +0300

    Add new properties and initializer to delegate

--- ios/Classes/BetterPlayerEzDrmAssetsLoaderDelegate.h
+++ ios/Classes/BetterPlayerEzDrmAssetsLoaderDelegate.h
@@ -1,3 +1,20 @@
+// // Copyright 2017 The Chromium Authors. All rights reserved.
+// // Use of this source code is governed by a BSD-style license that can be
+// // found in the LICENSE file.
+
+// #import <Foundation/Foundation.h>
+// #import <AVKit/AVKit.h>
+// #import <AVFoundation/AVFoundation.h>
+
+// @interface BetterPlayerEzDrmAssetsLoaderDelegate : NSObject
+
+// @property(readonly, nonatomic) NSURL* certificateURL;
+// @property(readonly, nonatomic) NSURL* licenseURL;
+// - (instancetype)init:(NSURL *)certificateURL withLicenseURL:(NSURL *)licenseURL;
+
+// @end
+
+
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -10,6 +27,14 @@
 
 @property(readonly, nonatomic) NSURL* certificateURL;
 @property(readonly, nonatomic) NSURL* licenseURL;
-- (instancetype)init:(NSURL *)certificateURL withLicenseURL:(NSURL *)licenseURL;
+@property(readonly, nonatomic) NSDictionary* headers;
+@property(readonly, nonatomic) NSString* videoId;
+@property(readonly, nonatomic) NSString* libraryId;
+
+- (instancetype)init:(NSURL *)certificateURL 
+     withLicenseURL:(NSURL *)licenseURL 
+            headers:(NSDictionary *)headers
+            videoId:(NSString *)videoId 
+          libraryId:(NSString *)libraryId;
 
 @end
